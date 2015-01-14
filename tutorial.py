c124a195f1e9e8d6009374ddc33ee41171d1faa9
Julia Cochran <juliacochran7@gmail.com>
2015-01-14 15:45:46 -0500

    Added function outlines

---
+++ tutorial.py
@@ -0,0 +1,20 @@
+class Tutorial:
+	""" Basic python class """
+
+	def function_one(self):
+		print("I am function one.")
+
+	def function_two(self):
+		print("I am function two.")
+
+	def function_three(self):
+		print("I am function three.")
+
+	def function_four(self):
+		print("I am function four.")
+
+	def function_five(self):
+		print("I am function five.")
+
+	def group_function(self):
+		print("everybody change me.")
